0e87ea6b326d189332a0fa96c8ce296af9042b98
Eishan Lawrence <eishanlawrence5@gmail.com>
2019-09-21 21:22:47 -0700

    Starter Code

---
+++ src/main/java/com/eishanlawrence/App.java
@@ -0,0 +1,13 @@
+package com.eishanlawrence;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
