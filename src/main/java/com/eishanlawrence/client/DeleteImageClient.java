0ab5e90c51cd5f7ebbf613b07c5321dd64bf8a4e
Eishan Lawrence <eishanlawrence5@gmail.com>
2019-09-26 19:21:49 -0700

    Implemented DeleteImage RPC

---
+++ src/main/java/com/eishanlawrence/client/DeleteImageClient.java
@@ -0,0 +1,36 @@
+package com.eishanlawrence.client;
+
+import com.eishanlawrence.ImageHub;
+import com.eishanlawrence.ImageServiceGrpc;
+import com.eishanlawrence.UserServiceGrpc;
+import io.grpc.ManagedChannel;
+import io.grpc.ManagedChannelBuilder;
+
+public final class DeleteImageClient {
+  private static final String IMAGE_URL =
+      "https://www.petmd.com/sites/default/files/diarrhea-long-term-dogs.jpg";
+  private static final String EMAIL = "random@random.com";
+
+  public static void main(String[] args) throws Exception {
+    final ManagedChannel channel =
+        ManagedChannelBuilder.forTarget("localhost:8080").usePlaintext().build();
+    // Create one stub for uploading image, and one stub for creating a user
+    ImageServiceGrpc.ImageServiceBlockingStub imageStub = ImageServiceGrpc.newBlockingStub(channel);
+    System.out.println("Now sending request to upload image");
+    ImageHub.DeleteImageRequest request =
+        ImageHub.DeleteImageRequest.newBuilder()
+            .setApiKey(ImageHub.AuthKey.newBuilder().setValue("8KHYc4Yetx8YLDQ42l7O").build())
+            .setEmail(EMAIL)
+            .setImage(
+                ImageHub.ImageUrl.newBuilder()
+                    .setImageName("Mad Scientist")
+                    .setUrl(IMAGE_URL)
+                    .build())
+            .build();
+    ImageHub.DeleteImageResponse response = imageStub.deleteImage(request);
+    System.out.println("success is: " + response.getSuccess());
+    channel.shutdown();
+  }
+
+  private DeleteImageClient() {}
+}
--- src/main/java/com/eishanlawrence/utils/ImageUtilities.java
+++ src/main/java/com/eishanlawrence/utils/ImageUtilities.java
@@ -1,5 +1,7 @@
 package com.eishanlawrence.utils;
 
+import com.eishanlawrence.ImageHub;
+import com.google.cloud.firestore.DocumentSnapshot;
 import com.google.protobuf.ByteString;
 
 import javax.imageio.IIOImage;
@@ -14,34 +16,35 @@ import java.net.URL;
 import java.util.Iterator;
 
 public final class ImageUtilities {
-    private static final String JPG = "jpg";
+  private static final String JPG = "jpg";
 
-    /**
-     * Given a URL, returns a buffered image
-     */
-    public static BufferedImage getImageFromUrl(String imageUrl) throws IOException {
-        URL url = new URL(imageUrl);
-        return ImageIO.read(url);
-    }
+  /** Given a URL, returns a buffered image */
+  public static BufferedImage getImageFromUrl(String imageUrl) throws IOException {
+    URL url = new URL(imageUrl);
+    return ImageIO.read(url);
+  }
 
-    /**
-     * Compresses and converts a Buffered Image to ByteString
-     */
-    public static ByteString imageToByteString(BufferedImage image) throws IOException {
-        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+  /** Compresses and converts a Buffered Image to ByteString */
+  public static ByteString imageToByteString(BufferedImage image) throws IOException {
+    ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
 
-        Iterator<ImageWriter> writers = ImageIO.getImageWritersByFormatName("jpg");
-        ImageWriter writer = writers.next();
-        ImageOutputStream ios = ImageIO.createImageOutputStream(byteArrayOutputStream);
-        writer.setOutput(ios);
-        ImageWriteParam param = writer.getDefaultWriteParam();
-        param.setCompressionMode(ImageWriteParam.MODE_EXPLICIT);
-        param.setCompressionQuality(0.0005f);  // Change the quality value you prefer
-        writer.write(null, new IIOImage(image, null, null), param);
+    Iterator<ImageWriter> writers = ImageIO.getImageWritersByFormatName("jpg");
+    ImageWriter writer = writers.next();
+    ImageOutputStream ios = ImageIO.createImageOutputStream(byteArrayOutputStream);
+    writer.setOutput(ios);
+    ImageWriteParam param = writer.getDefaultWriteParam();
+    param.setCompressionMode(ImageWriteParam.MODE_EXPLICIT);
+    param.setCompressionQuality(0.0005f); // Change the quality value you prefer
+    writer.write(null, new IIOImage(image, null, null), param);
 
-        ImageIO.write(image, JPG, byteArrayOutputStream);
-        return ByteString.copyFrom(byteArrayOutputStream.toByteArray());
-    }
+    ImageIO.write(image, JPG, byteArrayOutputStream);
+    return ByteString.copyFrom(byteArrayOutputStream.toByteArray());
+  }
 
-    private ImageUtilities() {}
+  /** Given a document snapshot, return true if it matches the given image url */
+  public static String urlToId(ImageHub.ImageUrl url) {
+    return url.getUrl().replace("/", "");
+  }
+
+  private ImageUtilities() {}
 }
